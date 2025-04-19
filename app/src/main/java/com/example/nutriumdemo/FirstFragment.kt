58092cfdb07d36aad7181b187361b1b49550ef2b
isabelcouto <isabelsantoscouto@gmail.com>
2025-04-19 06:54:38 +0100

    Drop down menu

--- app/src/main/java/com/example/nutriumdemo/FirstFragment.kt
+++ app/src/main/java/com/example/nutriumdemo/FirstFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -45,7 +49,25 @@ class FirstFragment : Fragment(), OnProfessionalClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val spinner: Spinner = view.findViewById(R.id.spinnerDropdown)
 
+        val options = listOf("Best for you", "Rating", "Most popular")
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val itemSelecionado = parent.getItemAtPosition(position).toString()
+                Toast.makeText(requireContext(), "Selecionou: $itemSelecionado", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // nada selecionado
+            }
+        }
 
         GlobalScope.launch {
             var lista = ProfessionalsRepository.getInstance().getProfessionals()
